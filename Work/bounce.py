6ff8b6ad72bb6b38dbbc1a9f7c2b7b94a6a0bb72
Geng Chen <cg@Gengs-Mac-mini.lan>
2020-06-13 11:07:31 +0800

    ex 1.5

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,14 @@
 # bounce.py
 #
 # Exercise 1.5
+
+# a rubber ball dropped from a height of 100 meters and and each time it hits the ground, i
+#t bounces back up to 3/5 the height it fell. 
+#Write a program bounce.py that prints a table showing the height of the first 10 bounces.
+hitNo=0
+height=100
+rebounce = height
+while hitNo<10:
+    rebounce = rebounce * 3/5
+    print(hitNo + 1, round(rebounce,4))
+    hitNo = hitNo +1

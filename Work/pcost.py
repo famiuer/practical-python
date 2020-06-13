9fe88cdeea0d074ffd6ba1f6da345b0e692e4dbc
Geng Chen <cg@Gengs-Mac-mini.lan>
2020-06-13 17:20:09 +0800

    finish chapter 1

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,25 @@
 # pcost.py
 #
 # Exercise 1.27
+
+def portfolio_cost(filename):
+    f = open(filename, 'rt')
+
+    headers = next(f).split(',')
+    #print(headers)
+    sum = 0
+    for line in f:
+        row = line.split(',')
+        #print(row)
+        sum = sum + int(row[1]) * float(row[2])
+    f.close()
+    print(f'Total cost {sum}')
+
+import sys 
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+cost = portfolio_cost(filename)
+#print('Total cost:', cost)
